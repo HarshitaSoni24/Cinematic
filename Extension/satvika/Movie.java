071d1ac8840742e784dc50360d69099ee1b85be7
joongdok1864 <137913097+joongdok1864@users.noreply.github.com>
2024-11-19 11:37:00 +0530

    Update Movie.java

--- Extension/satvika/Movie.java
+++ Extension/satvika/Movie.java
@@ -1,11 +1,16 @@
 package src.pkg;
 
-public class Movie {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie implements Serializable {
     private String title;
     private String genre;
     private int duration; // Duration in minutes
     private double rating; // Rating out of 10
     private double price; // Price of the ticket
+    private List<Theatre> theatres; // List of associated theatres
 
     // Constructor
     public Movie(String title, String genre, int duration, double rating, double price) {
@@ -14,47 +19,27 @@ public class Movie {
         this.duration = duration;
         this.rating = rating;
         this.price = price;
+        this.theatres = new ArrayList<>(); // Initialize to prevent null pointer issues
     }
 
     // Getters and Setters
-    public String getTitle() {
-        return title;
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getGenre() { return genre; }
+    public void setGenre(String genre) { this.genre = genre; }
+    public int getDuration() { return duration; }
+    public void setDuration(int duration) { this.duration = duration; }
+    public double getRating() { return rating; }
+    public void setRating(double rating) { this.rating = rating; }
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+
+    public List<Theatre> getTheatres() {
+        return theatres;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public double getRating() {
-        return rating;
-    }
-
-    public void setRating(double rating) {
-        this.rating = rating;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
+    public void setTheatres(List<Theatre> theatres) {
+        this.theatres = theatres;
     }
 
     @Override
